4fd900c918c205de243e3d4af8987278d790b9f1
a22angelgp <a22angelgp@iessanclemente.net>
2023-11-04 19:24:40 +0100

    (+,*) Register & Update Main

---
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uf1_proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
