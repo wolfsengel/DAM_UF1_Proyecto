39f1cacecbbe8e836bef3e8cf73e37a777f0914c
a22angelgp <a22angelgp@iessanclemente.net>
2023-11-07 16:46:03 +0100

    (*) great adjustment

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/Activity/IntroActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/Activity/IntroActivity.kt
@@ -1,12 +1,21 @@
 package com.example.uf1_proyecto.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.*
 import com.example.uf1_proyecto.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        val getInButton: AppCompatButton = findViewById(R.id.getInButton)
+
+        getInButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/Activity/LoginActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/Activity/LoginActivity.kt
@@ -1,12 +1,36 @@
 package com.example.uf1_proyecto.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 import com.example.uf1_proyecto.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var userTxt: EditText
+    private lateinit var passwordTxt: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        initView()
+    }
+
+    private fun initView() {
+        userTxt = findViewById(R.id.usernameTxt)
+        passwordTxt = findViewById(R.id.passwordTxt)
+        loginButton = findViewById(R.id.login_button)
+        registerButton = findViewById(R.id.register_button)
+
+        loginButton.setOnClickListener {
+            if (userTxt.text.toString().isEmpty() || passwordTxt.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            } else if (userTxt.text.toString() == "test" && passwordTxt.text.toString() == "test") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
